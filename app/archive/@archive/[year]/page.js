c45202140b6b71c25dbaef01ce7e013cf0fa95eb
majidH <majid.hadadian@gmail.com>
2024-11-19 10:53:33 +0330

    Edit achive page

--- app/archive/@archive/[year]/page.js
+++
@@ -1,8 +0,0 @@
-import NewsList from "@/components/news-list";
-import { getAvailableNewsYears, getNewsForYear } from "@/lib/news";
-
-export default function FilteredNewsPage({ params }) {
-  const year = params.year;
-  const currentNews = getNewsForYear(year);
-  return <NewsList news={currentNews} />;
-}
